da75abe694ead96db710311edaf1e585913a7f92
Zhang Qingruo <1106691301@qq.com>
2023-03-16 11:51:04 +0800

    add dataset

--- train.py
+++ train.py
@@ -106,7 +106,7 @@ def train_model(
 
                 with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
                     masks_pred = model(images)
-                    if model.n_classes == 1:
+                    if n_classes == 1:
                         loss = criterion(masks_pred.squeeze(1), true_masks.float())
                         loss += dice_loss(F.sigmoid(masks_pred), true_masks.float(), multiclass=False)
                     else:
